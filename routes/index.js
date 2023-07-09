7f84a1f6abb67c0f77f9cbd24cc538e9be56b4d4
Git-Hub452 <paudelsabina345@gmail.com>
2023-07-10 02:16:09 +0545

    edit patient details

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var patientinfo= require("../model/database");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/add", function (req, res, next) {
+  patientinfo.find().then((patientDetails) => {
+    res.render("dashboard", {patientDetails:patientDetails});
+  });
 });
 
 router.get('/hospital', function(req, res, next) {
@@ -12,6 +13,11 @@ router.get('/hospital', function(req, res, next) {
 }); 
 router.get('/hospitaldetail', function(req, res, next) {
   res.render('hospitaldetail',{hospitalName:'lumbiniTechnicalHopital', specialistNamesList: ["Ram", "Shyam"], specilities:"orthopedic"});
+});
+
+
+router.get('/appointmentform', function(req, res, next) {
+  res.render('appointmentForm');
 });
 
 router.post("/saveappointment",  async function (req, res, next) {
@@ -27,24 +33,59 @@ router.post("/saveappointment",  async function (req, res, next) {
       "appoitmentTime": req.body.appoitmentTime,
     }
   );
-  // console.log(formData);
-  // expenses.push({ ...formData, _id: expenses.length + 1 });
-  // res.redirect("/");
 
    await patientinfo.insertMany(formData) 
-    res.redirect('/');
+    res.redirect('/add');
   
 });
 
-router.get('/appointmentform', function(req, res, next) {
-  res.render('appointmentForm');
-});
-router.post('/saveappointment', function(req, res, next) {
-  res.render('index');
+router.get("/edit/:id", async function (req, res) {
+  debugger
+  const patientDetails =  await patientinfo.findOne(
+    { _id: req.params.id}
+    );
+
+  let appointmentDate = patientDetails.appointmentDate;
+  if (!appointmentDate) appointmentDate = new Date();
+
+  const formattedAppointmentDate = [
+    appointmentDate.getFullYear(),
+    (appointmentDate.getMonth() + 1).toString().padStart(2, '0'),
+    appointmentDate.getDate().toString().padStart(2, '0')
+  ].join('-');
+
+  let dateOfBirth = patientDetails.DateOfBirth;
+  if (!dateOfBirth) dateOfBirth = new Date();
+
+  const formattedBirthDate = [
+    dateOfBirth.getFullYear(),
+    (dateOfBirth.getMonth() + 1).toString().padStart(2, '0'),
+    dateOfBirth.getDate().toString().padStart(2, '0')
+  ].join('-');
+
+  res.render("editAppointment", { patientDetails: patientDetails, formattedAppointmentDate, formattedBirthDate });
 });
 
-router.get ('/add',function(req,res){
-  res.render('dashboard')
+
+
+router.post("/saveEdited/:id", async function (req, res) {
+ let formData= {
+       FullName: req.body.FullName,
+   Gender: req.body.Gender,
+   DateOfBirth: req.body.DateOfBirth,
+    Age: req.body.Age,
+    Email:req.body.Email,
+ PhoneNumber: req.body.PhoneNumber,
+appoitmentDate: req.body.appoitmentDate,
+  appoitmentTime: req.body.appoitmentTime
+ };
+await Expenses.findOneAndUpdate(
+   {_id: req.params.id},
+  { $set:{ ...formData } }
+
+
+  );
+  res.redirect('/')
 });
 
 module.exports = router;
